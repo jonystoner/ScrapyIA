e8abd5555f6e2e8e109a4f968b184caed0a40f4a
jonystoner <jony5851@gmail.com>
2023-02-09 18:35:57 -0300

    firt commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="bg-black w-screen h-screen">
+        <div className="flex flex-row justify-between">
+          <div className="text-white ml-5 mt-5 text-4xl"> ScrapyAI</div>
+          <div className=" text-white mr-10 mt-5 flex flex-row gap-4">
+            <button>Home</button>
+            <button>contatos</button>
+            <button>Login</button>
+          </div>
+        </div>
+        <div className="w-screen h-2 flex flex-row bg-white mt-4"></div>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
