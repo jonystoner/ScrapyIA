7ca634260a9da3f653c1264acc4bfbed90dd0b3d
jonystoner <jony5851@gmail.com>
2023-09-14 14:40:25 -0300

    ajustes no login e nos users

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { usersPost } from "../hooks/createHoks";
@@ -14,85 +13,77 @@ export function Login() {
   const [password, setPassword] = useState("");
 
   function createLogin() {
-    if (!name) {
+    if (name.length < 1) {
       alert("O Campo nome é obrigatorio");
     } else if (!email.length) {
       alert("Campo Email obrigatorio");
     } else if (!password.length) {
-      alert("Campo Senha obrigatorio");   
-    } else if(name|| email || password){
-      usersPost(name, email, password)
-      alert("Criado com sucesso")
-      navigate("/")
-    } 
-    else {
-      console.error(Error);
-    }   
-   
+      alert("Campo Senha obrigatorio");
+    } else if (name || email || password) {
+      usersPost(name, email, password);
+      alert("Criado com sucesso");
+      navigate("/");
+    } else {
+      console.error(`erro ${Error}`);
+    }
   }
 
-  useEffect(() => {    
-
-  },[])
+  useEffect(() => {});
 
   return (
     <>
-      <div className="bg-black w-auto h-screen">
+      <div className="bg-black w-screen h-screen">
         <Navbar />
         <ul className="w-auto h-2 bg-white mt-5" />
-        <div className="flex flex-col ml-14">
+        <div className="flex flex-row justify-center w-screen">
           <h1 className="text-white text-4xl mt-5 text-ellipsis">
             Crie Seu Login
           </h1>
         </div>
-        <form className=" mt-6 ml-10  w-auto flex flex-row">
-          <label className="flex flex-col text-white">
-            Nome:
-            <input
-              type="text"
-              className="w-96 rounded-md border-2 border-gray-700 text-center text-black"
-              placeholder="Nome"
-              onChange={(event) => setName(event.target.value)}
-              value={name}
-            />
-          </label>
-          <label className=" text-white flex flex-col ml-5">
-            EmaiL:
-            <input
-              type="text"
-              className="w-96 rounded-md border-2 border-gray-700 text-center text-black"
-              placeholder="Email"
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
-            />
-          </label>
-        </form>
-        <form className="flex flex-row  gap-5 ml-10 mt-7 w-24 h-8">
-          <label className="text-white">
-            Whathsap:
-            <input
-              type="text"
-              className="w-96 rounded-md border-2 border-gray-700 text-center text-black"
-              placeholder="Whathsap"
-              onChange={(event) => setPassword(event.target.value)}
-              value={password}
-            />
-          </label>
-          <label className="text-white">
-            Senha:
-            <input
-              type="text"
-              className="w-96 rounded-md border-2 border-gray-700 text-center text-black"
-              placeholder="senha"
-            />
-          </label>
-        </form>
-        <div className="flex flex-row justify-end w-1/2 mt-10">
+        <div className="flex justify-center w-auto h-auto">
+          <div className="flex flex-row">
+            <div className=" mt-10 gap-8 flex flex-col">
+              <label className="flex flex-row gap-4 text-white">
+                Nome:
+                <input
+                  type="text"
+                  className="w-96 h-11 rounded-md border-2 border-gray-700 text-center text-black"
+                  placeholder="Nome"
+                  onChange={(event) => setName(event.target.value)}
+                  value={name}
+                />
+              </label>
+              <label className="flex flex-row gap-4 text-white">
+                EmaiL:
+                <input
+                  type="text"
+                  className="w-96 h-11 rounded-md border-2 border-gray-700 text-center text-black"
+                  placeholder="Email"
+                  onChange={(event) => setEmail(event.target.value)}
+                  value={email}
+                />
+              </label>
+              <label className="flex flex-row mt-1 gap-4 text-white">
+                Senha:
+                <input
+                  type="text"
+                  className="w-96 h-11 rounded-md border-2 border-gray-700 text-center text-black"
+                  placeholder="*********"
+                  onChange={(event) => setPassword(event.target.value)}
+                  value={password}
+                  autoComplete="current-password"
+                />                
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row justify-end ">
           <button
-            className="w-12 h-12 rounded-md bg-white"
+            className="w-12 h-12 rounded-md bg-green-300 text-white"
             onClick={createLogin}
           >
-            criar
+            {" "}
+            Salvar
           </button>
         </div>
       </div>
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -2,40 +2,41 @@ import { useEffect, useState } from "react";
 import { api } from "../hooks/createHoks";
 import { CreateUser } from "../interface/Record";
 
-import DataTable, { TableColumn }   from "react-data-table-component";
+import DataTable, { TableColumn } from "react-data-table-component";
 
 import { Navbar } from "../components/Navbar";
 
-
-export function Users(): JSX.Element  {
+export function Users(): JSX.Element {
   const [dados, setDados] = useState<CreateUser[]>([]);
 
-  
+  const date = new Date();
+
+  const formattedDate = date.toLocaleDateString("en-GB");
 
   const columns: TableColumn<CreateUser>[] = [
     {
-        name: 'Nome',
-        cell: row => row.name,
-        style:{
-          background: "blue",
-        }
+      name: "Nome",
+      cell: (row) => row.name,
+      style: {
+        background: "blue",
+      },
     },
     {
-        name: 'Email',
-        cell: row => row.email,
-        style:{
-          background: "red",
-        }        
+      name: "Email",
+      cell: (row) => row.email,
+      style: {
+        background: "red",
+      },
     },
 
     {
-      name: 'Data de Criação',
-      cell: row => row.createdAt.replace("",""),
-      style:{
+      name: "Data de Criação",
+      cell: (row) => row.createdAt.trimStart(),
+      style: {
         background: "green",
-      }      
-  },
-];
+      },
+    },
+  ];
 
   useEffect(() => {
     api.get("/users", {}).then((response) => {
@@ -48,13 +49,14 @@ export function Users(): JSX.Element  {
       <div className="w-screen h-screen bg-black">
         <Navbar />
         <ul className="w-auto h-2 bg-white mt-5" />
-        <div className="h-auto mt-10 ml-5 mr-5 rounded-t-sm border-2 border-red-600">
+        <div className=" flex flex-col h-auto mt-10 ml-5 mr-5 rounded-t-sm border-2 border-red-600">
+          <div className="text text-white"> {formattedDate} </div>
           <DataTable
             title="Daddos dos Usuários"
             data={dados}
             columns={columns}
             pagination
-            selectableRows           
+            selectableRows
           />
         </div>
       </div>
